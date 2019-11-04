bc37baf53f0de4f0a10e6aa4b83baf1ea2ced783
MarvelNwachukwu <marvellousn561@gmail.com>
2019-11-04 23:05:13 +0100

    Designed the structure with CSS

---
+++ MyScript.js
@@ -0,0 +1,36 @@
+const calculator = document.querySelector(‘.calculator’)
+const keys = calculator.querySelector(‘.calculator__keys’)
+
+keys.addEventListener(‘click’, e => {
+ if (e.target.matches(‘button’)) {
+   // Do something
+ }
+})
+
+const key = e.target
+const action = key.dataset.action
+
+if (!action) {
+    console.log('number key!')
+  }
+
+  f (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    console.log('operator key!')
+  }
+
+  if (action === 'decimal') {
+    console.log('decimal key!')
+  }
+  
+  if (action === 'clear') {
+    console.log('clear key!')
+  }
+  
+  if (action === 'calculate') {
+    console.log('equal key!')
+  }
