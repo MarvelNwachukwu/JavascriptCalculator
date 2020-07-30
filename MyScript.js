80acd2a78d312b136f5c16084c46c9da772fa011
MornsCanCode <marvellousn561@gmail.com>
2020-07-30 13:28:40 +0100

    Made the calculator listen and display user inputs
    A basic JS calculator

--- MyScript.js
+++ MyScript.js
@@ -1,1 +1,20 @@
-alert
+ const screen = document.getElementsByClassName('calculator__display');
+ const keys = document.querySelectorAll('#funcKey');
+ let onScreen = '';
+
+// This chunk of code is listening to keys
+
+keys.forEach(item => {
+  item.addEventListener('mouseup', () => {
+    onScreen += item.innerHTML;
+    screen[0].innerHTML = onScreen;
+  })
+})
+
+// This chunk of code is listening to AC key and clears the screen when triggered
+
+document.querySelector('.clear').addEventListener('mouseup', () => {
+  screen[0].innerHTML = 0;
+  onScreen = '';
+})
+
