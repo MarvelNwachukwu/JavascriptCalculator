8665cef91b17e778e85662e15af59d9f3b95c04d
MornsCanCode <marvellousn561@gmail.com>
2020-08-05 06:58:34 +0100

    second commit

--- MyScript.js
+++ MyScript.js
@@ -1,13 +1,16 @@
- const screen = document.getElementsByClassName('calculator__display');
- const keys = document.querySelectorAll('#funcKey');
- let onScreen = '';
+const screen = document.getElementsByClassName('calculator__display');
+const keys = document.querySelectorAll('#funcKey');
+let onScreen = '';
+let operator;
 
-// This chunk of code is listening to keys
+// This chunk of code is listening to numeric keys
 
 keys.forEach(item => {
   item.addEventListener('mouseup', () => {
-    onScreen += item.innerHTML;
+    onScreen += parseInt(item.innerHTML);
     screen[0].innerHTML = onScreen;
+    // console.log(`The user clicked ${item.innerHTML}, onScreen now contains ${onScreen.length} items`)
+    // console.log(onScreen)
   })
 })
 
@@ -16,5 +19,38 @@ keys.forEach(item => {
 document.querySelector('.clear').addEventListener('mouseup', () => {
   screen[0].innerHTML = 0;
   onScreen = '';
+  // console.log(`The user cleared input, onScreen now contains ${onScreen.length} items`)
 })
 
+// This blocks of code listens to operator keys
+
+document.querySelectorAll('#KeyOperator').forEach(item => {
+  item.addEventListener('mouseup', () => {
+    console.log(item.innerHTML)
+    onScreen += item.innerHTML
+
+    // if(item.innerHTML == '+'){
+    //   operator = +;
+    // }else if(item.innerHTML == '-'){
+    //   operator = -
+    // }else if(item.innerHTML == '*'){
+    //   operator = *
+    // }else if(item.innerHTML == '÷'){
+    //   operator = ÷
+    // }
+
+  })
+})
+
+// const calculate = () => {
+
+// }
+
+
+
+
+// This chunk of code is listens to Equals to key and calculates when triggered
+
+document.querySelector('.KeyEqual').addEventListener('mouseup', () => {
+  
+})
