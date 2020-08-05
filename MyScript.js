c216ece41cb86899e225a006c942cd726bf9c7d4
MornsCanCode <marvellousn561@gmail.com>
2020-08-05 10:45:18 +0100

    3/4 operands working

--- MyScript.js
+++ MyScript.js
@@ -1,7 +1,10 @@
 const screen = document.getElementsByClassName('calculator__display');
 const keys = document.querySelectorAll('#funcKey');
 let onScreen = '';
+let whatToProcess = [];
+let userCurrentInput = 0;
 let operator;
+let result;
 
 // This chunk of code is listening to numeric keys
 
@@ -9,8 +12,21 @@ keys.forEach(item => {
   item.addEventListener('mouseup', () => {
     onScreen += parseInt(item.innerHTML);
     screen[0].innerHTML = onScreen;
-    // console.log(`The user clicked ${item.innerHTML}, onScreen now contains ${onScreen.length} items`)
-    // console.log(onScreen)
+    userCurrentInput += item.innerHTML;
+    // console.log(userCurrentInput)
+  })
+})
+
+// This blocks of code listens to operator keys and stores user inputs in whatToProcess
+
+document.querySelectorAll('#KeyOperator').forEach(item => {
+  item.addEventListener('mouseup', () => {
+    operator = item.innerHTML
+    onScreen += item.innerHTML
+    whatToProcess.push(userCurrentInput)
+    userCurrentInput = 0
+    console.log(whatToProcess)
+
   })
 })
 
@@ -19,38 +35,32 @@ keys.forEach(item => {
 document.querySelector('.clear').addEventListener('mouseup', () => {
   screen[0].innerHTML = 0;
   onScreen = '';
-  // console.log(`The user cleared input, onScreen now contains ${onScreen.length} items`)
+  whatToProcess = [];
+  operator = 0;
+  result = 0;
+  userCurrentInput = 0;
 })
-
-// This blocks of code listens to operator keys
-
-document.querySelectorAll('#KeyOperator').forEach(item => {
-  item.addEventListener('mouseup', () => {
-    console.log(item.innerHTML)
-    onScreen += item.innerHTML
-
-    // if(item.innerHTML == '+'){
-    //   operator = +;
-    // }else if(item.innerHTML == '-'){
-    //   operator = -
-    // }else if(item.innerHTML == '*'){
-    //   operator = *
-    // }else if(item.innerHTML == '÷'){
-    //   operator = ÷
-    // }
-
-  })
-})
-
-// const calculate = () => {
-
-// }
-
-
-
 
 // This chunk of code is listens to Equals to key and calculates when triggered
 
 document.querySelector('.KeyEqual').addEventListener('mouseup', () => {
-  
+  whatToProcess.push(userCurrentInput)
+  // for(let i = 0; i < whatToProcess.length; i++){
+  //   parseInt(whatToProcess[i])
+  //   console.log(whatToProcess[i])
+  //   console.log(typeof(parseInt(whatToProcess[i])))
+  // }
+
+
+  if (operator == '+') {
+    result = parseInt(whatToProcess[0]) + parseInt(whatToProcess[1])
+  } else if (operator == '-') {
+    result = parseInt(whatToProcess[0]) - parseInt(whatToProcess[1])
+  } else if (operator == '*') {
+    result = parseInt(whatToProcess[0]) * parseInt(whatToProcess[1])
+  } else if (operator == '÷') {
+    result = parseInt(whatToProcess[0]) / parseInt(whatToProcess[1])
+  }
+  console.log(result)
+  screen[0].innerHTML = result;
 })
